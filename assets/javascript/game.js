1721cdf8cbedee5c4404bbeab9b5019ae9a69601
Mar <marcianelli.m@att.net>
2017-02-24 18:49:04 -0800

    final edits

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,13 @@ var counter = 0;                               //track users clicks on crystals 
 var numOfcrystals = [1, 2, 3, 4];              //create 4 crystals
 var minNum = 19;                               //number to match must be between 19-120
 var maxNum = 120;
-var targetNumber = randomNum(minNum, maxNum);  ///this is the number they need to match
-var crystalValue = [];                         //hold the value of each crystal here
+  
+var crystalValue = [];                        //hold the value of each crystal here
+
 var wins = 0;
 var losses = 0;
 var currentScore = [];
+
 var myImages = [
     "assets/images/madhatter.png",
     "assets/images/cheshirecat.png",
@@ -15,13 +17,17 @@ var myImages = [
     "assets/images/whiterabbit.png",
   ]; 
 
-//$(document).ready(function(){}  //wrap this around all of the functions when they work 
 
 
+
+
+  //create a random number.
+  // then put that number on my html page
+  //return ends function and specifies values
   function randomNum (min,max){
     return Math.floor(Math.random()*(max-min+1)+min);
   }
-
+  var targetNumber = randomNum(minNum, maxNum);
   $("#number-to-guess").html(targetNumber);
 
 
@@ -37,57 +43,60 @@ var myImages = [
     imageCrystal.attr("data-crystalvalue", Math.floor(Math.random() * 12) + 1);
     $(".crystals").append(imageCrystal);
     }
-
-    // Add the crystalValue to the user's "counter" which is a global variable.
-    // Every click, from every crystal adds to the global counter.
+                                                      //$(".crystals").empty();   to empty the value of the crystals
+  // Add the crystalValue to the user's "counter" which is a global variable.
+  // Every click, from every crystal adds to the global counter.
   $(".crystal-image").on("click", function() {    
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
     counter += crystalValue;
 
     //show their current score here  ("New score: " + counter);
-    $("#currentScore").text("Your score:" + counter);
+    $("#currentScore").text("Your score: " + counter);
 
     if (counter === targetNumber) {
       wins++;
       $("#wins").text('Wins: ' + wins);
       alert("You win!");
-      randomNum();             ////why doesnt this give a new random number????
+      resetVariables();
+       
     }
 
     else if (counter > targetNumber) {
       losses++;
       $("#losses").text('Losses: ' + losses);
       alert("You lose!");
-      randomNum();             ////why doesnt this give a new random number????
-
-  console.log(crystalValue);
-  console.log(counter);
-  console.log(targetNumber);
+      resetVariables();
+       
       }
 
   });
 
 
-//write a function? or something that resets number to match
+//write a function? or something that resets number to match and the counter
 //resets value of each crystal
 //resets their score to 0
 //but keeps wins and losses (so dont touch them)
 
- // $("#restart").on("click", function(){
- //    $(".currentScore, .counter, .targetNumber, .crystalValue").appendTo(".reset");
- //    randomNum(min,max);
- //    console.log("test");
- //  });
+  function resetVariables(){
+    randomNum();
+    var targetNumber = randomNum(minNum, maxNum);
+    $("#number-to-guess").html(targetNumber);                                    
+    currentScore = [];
+    counter = 0;
+    crystalValue = [];
+  }
+
+  
 
 
-//function resetVariables(){
-        //return randomNum(min,max);
-        //return currentScore = [];
-        //return counter = 0;
-       
-        //}
+  
 
+
+ 
+ 
+
+ 
 
 
 
